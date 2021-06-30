bb91a90325249def6eabe1a1cce229b3769e8646
Neyune <ereali@e1r11p22.42.fr>
2021-06-30 15:06:22 +0200

    rate sujet

--- srcs/ErrorGest.c
+++ srcs/ErrorGest.c
@@ -6,7 +6,7 @@
 /*   By: ereali <ereali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/02 17:47:35 by ereali            #+#    #+#             */
-/*   Updated: 2021/06/30 12:36:51 by ereali           ###   ########.fr       */
+/*   Updated: 2021/06/30 15:02:25 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,20 +52,31 @@ int     *atoitab(char **tab)
     int i;
 
     i = 0;
-    lst = 0;
 
-    while (tab[i])
-    {
-        printf("s\n", tab[i]);
-        printf("%d\n",ft_atoi(tab[i]));
-    }
     /*while (tab[i])
     {
-        lst = (int *)malloc(sizeof (int));
-        lst[i] = ft_atoi(tab[i]);
+        printf("%s\n", tab[i]);
+        printf("%d\n",ft_atoi(tab[i]));
         i++;
-        printf("%d\n", lst[i]);
     }*/
+    while (i < strlentab(tab))
+    {
+//    printf("%d\n", i);
+        lst = (int *)malloc(sizeof (int) * strlentab(tab));
+        printf("string avant = %s\n", tab[i]);
+        lst[i] = ft_atoi(tab[i]);
+        printf("int avant %d\n", lst[i]);
+
+        i++;
+        //printf("%d\n", lst[i-1]);
+    }
+    i = 0 ;
+    while (i < strlentab(tab))
+    {
+        // printf("int = %d\n", lst[i]);
+        // printf("string = %s\n", tab[i]);
+        i++;
+    }
     return (lst);
 }
 
@@ -80,11 +91,12 @@ char    **ErrorGest(char *args)
         exit(EXIT_FAILURE);
     tab = ft_split(args,' ');
     lst = atoitab(tab);
-
-    /*while(lst[i])
-        printf("%i\n", lst[i]);
-
-    while (!(lst))
+    /*while (tab[i])
+    {
+        printf("%d\n", lst[i]);
+        i++;
+    }*/
+    /*while (!(lst))
     {
         *lst != lst[i];
         i++;
