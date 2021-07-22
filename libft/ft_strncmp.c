796a45c4643673df7ebfa3288b0487e8f39d7209
Neyune <ereali@e2r2p12.42.fr>
2021-07-23 00:42:56 +0200

    zero gest

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: ereali <ereali@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/08 12:25:47 by ereali            #+#    #+#             */
-/*   Updated: 2021/05/30 17:59:03 by ereali           ###   ########.fr       */
+/*   Updated: 2021/07/23 00:36:05 by ereali           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,19 @@
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
 	unsigned int	i;
+	unsigned int	j;
 
 	i = 0;
+	j = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] == s2[i] && s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (s1[i + 1] && s1[i] == '0')
 		i++;
+	while (s1[i] == s2[j] && s1[i] != '\0' && s2[j] != '\0' && i < n)
+	{
+		i++;
+		j++;
+	}
 	if (i >= n)
 		return (0);
 	return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
